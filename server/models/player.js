c7bec37427a14db9160413e3113a114562895617
AlanMedina53232 <42563570+AlanMedina53232@users.noreply.github.com>
2025-06-12 08:17:37 -0700

    se quita server

--- server/models/player.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const playerSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  score: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model("Player", playerSchema);
--- server/routes/playerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { saveScore } = require('../controllers/playerController');
-
-router.post('/', saveScore);
-
-module.exports = router;
